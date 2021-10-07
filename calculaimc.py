59f20b8ff94c2d809b58793439f8744bddc86f20
Natalha Santanna <natalha.ssantanna@live.com>
2021-10-07 01:58:29 -0300

    adicionei o codigo
    código cálculo imc. exercício proposto no curso de Lógica em Python

---
+++ calculaimc.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+nome_usuario = input('Digite seu nome: ')
+
+
+# In[28]:
+
+
+peso_usuario = float(input('Informe seu peso: '))
+
+
+# In[29]:
+
+
+altura_usuario = float(input('Informe sua altura: '))
+
+
+# In[30]:
+
+
+peso_usuario
+
+
+# In[31]:
+
+
+altura_usuario
+
+
+# In[32]:
+
+
+altura_ao_quadrado = altura_usuario * altura_usuario
+
+
+# In[33]:
+
+
+imc = altura_ao_quadrado
+
+
+# In[34]:
+
+
+print(imc)
+
+
+# In[ ]:
+
+
+
+
